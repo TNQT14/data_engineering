5b7081f5e467666d320cd5a6b270f83ef7519c75
Trương Nguyễn Quang Thái <2002quangthai@gmail.com>
2025-01-08 15:54:02 +0700

    getting data

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -6,3 +6,22 @@ default_args = {
     'owner' : 'airschoolar',
     'start_date': datetime(2025,1,1,10,10)
 }
+
+def stream_data():
+    import json
+    import requests
+
+    res = requests.get('https://randomuser.me/api/')
+    print(res.json())
+
+# with DAG('user_automation',
+#          default_args = default_args,
+#          schedule_interval= '@daily',
+#          catchup= False) as dag:
+#
+#     streaming_task = PythonOperator(
+#         task_id= 'stream_data_from_api',
+#         python_callable=stream_data
+#     )
+
+stream_data();
