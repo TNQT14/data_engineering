7fcd20cb90e198f781e854ecca4556e4bed8e790
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2025-01-13 10:48:59 +0700

    Getting data from API with Airflow

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from sqlalchemy_utils.types import uuid
 
 default_args = {
     'owner' : 'airschoolar',
@@ -11,11 +12,9 @@ def stream_data():
     import json
     import requests
 
-    res = requests.get('https://randomuser.me/api/')
-    data = res.json()
-    # Pretty-print the JSON
-    print(data)
-    print(json.dumps(data, indent=0))
+    res = get_data()
+    res = format_data(res)
+    print(json.dumps(res, indent=4))
 
 # with DAG('user_automation',
 #          default_args = default_args,
@@ -26,5 +25,32 @@ def stream_data():
 #         task_id= 'stream_data_from_api',
 #         python_callable=stream_data
 #     )
+#
+def get_data():
+    import requests
 
-stream_data();
+    res = requests.get('https://randomuser.me/api/')
+    res = res.json()
+    res = res['results'][0]
+    return res
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    # data['id'] = uuid.uuid4()
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
+stream_data()
