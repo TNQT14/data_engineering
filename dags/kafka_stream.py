42ebf0f4154bf441e7912c2d2b805202d8030221
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2025-01-16 16:33:24 +0700

    kafka setup & stream data

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,31 +1,46 @@
+import uuid
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from sqlalchemy_utils.types import uuid
 
 default_args = {
     'owner' : 'airschoolar',
     'start_date': datetime(2025,1,1,10,10)
 }
 
+i = 0
+
 def stream_data():
     import json
-    import requests
+    from kafka import KafkaProducer
+    import time
+    import logging
 
-    res = get_data()
-    res = format_data(res)
-    print(json.dumps(res, indent=4))
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
 
-# with DAG('user_automation',
-#          default_args = default_args,
-#          schedule_interval= '@daily',
-#          catchup= False) as dag:
-#
-#     streaming_task = PythonOperator(
-#         task_id= 'stream_data_from_api',
-#         python_callable=stream_data
-#     )
-#
+    while True:
+        if time.time() > curr_time + 5:  # 1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res, curr_time)
+
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
+
+with DAG('user_automation',
+         default_args = default_args,
+         schedule_interval= '*/10 * * * * *',
+         catchup= False) as dag:
+
+    streaming_task = PythonOperator(
+        task_id= 'stream_data_from_api',
+        python_callable=stream_data
+    )
+
 def get_data():
     import requests
 
@@ -34,7 +49,10 @@ def get_data():
     res = res['results'][0]
     return res
 
-def format_data(res):
+def format_data(res, curr_time):
+    import time
+    import datetime
+
     data = {}
     location = res['location']
     # data['id'] = uuid.uuid4()
@@ -51,6 +69,14 @@ def format_data(res):
     data['phone'] = res['phone']
     data['picture'] = res['picture']['medium']
 
+    # Convert to a datetime object
+    converted_time = datetime.datetime.fromtimestamp(time.time())
+
+    # Format the datetime object to a readable string
+    formatted_time = converted_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+    data['created_time'] = formatted_time
+    data['curr_time'] = datetime.datetime.fromtimestamp(curr_time).strftime("%Y-%m-%d %H:%M:%S.%f")
+
     return data
 
-stream_data()
+# stream_data()
