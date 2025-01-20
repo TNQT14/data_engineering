f27dade057a94a573bf8bd017751d623e11cfde9
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2025-01-20 13:58:49 +0700

    update

--- spark_stream.py
+++ spark_stream.py
@@ -18,11 +18,11 @@ def create_keyspace(session):
 
 def create_table(session):
     session.execute("""
-    DROP TABLE IF EXISTS spark_streams.created_users;
-    """)
+        DROP TABLE IF EXISTS spark_streams.created_users;
+        """)
     session.execute("""
     CREATE TABLE IF NOT EXISTS spark_streams.created_users (
-          id TEXT PRIMARY KEY,
+          id TEXT PRIMARY KEY, 
           first_name TEXT,
           last_name TEXT,
           gender TEXT,
@@ -41,8 +41,6 @@ def create_table(session):
 
 def insert_data(session, **kwargs):
     print("inserting data...")
-
-    user_id = kwargs.get('id')
     first_name = kwargs.get('first_name')
     last_name = kwargs.get('last_name')
     gender = kwargs.get('gender')
@@ -60,7 +58,7 @@ def insert_data(session, **kwargs):
             INSERT INTO spark_streams.created_users(id, first_name, last_name, gender, address, 
                 post_code, email, username, dob, registered_date, phone, picture)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """, (user_id, first_name, last_name, gender, address,
+        """, (first_name, last_name, gender, address,
               postcode, email, username, dob, registered_date, phone, picture))
         print(f"Data inserted for {first_name} {last_name}")
 
@@ -74,9 +72,10 @@ def create_spark_connection():
     try:
         s_conn = SparkSession.builder \
             .appName('SparkDataStreaming') \
-            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.12:3.5.0,"
+            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.12:3.5.1,"
                                            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1") \
             .config('spark.cassandra.connection.host', 'localhost') \
+            .config('spark.sql.shuffle.partitions', '1') \
             .getOrCreate()
 
         s_conn.sparkContext.setLogLevel("ERROR")
@@ -110,10 +109,10 @@ def create_cassandra_connection():
         cluster = Cluster(['localhost'])
 
         cas_session = cluster.connect()
-
+        print(f'check connect cluster {cas_session}')
         return cas_session
     except Exception as e:
-        logging.error(f"Could not create cassandra connection due to {e}")
+        print(f"Could not create cassandra connection due to {e}")
         return None
 
 
@@ -122,45 +121,22 @@ def create_selection_df_from_kafka(spark_df):
         StructField("id", StringType(), False),
         StructField("first_name", StringType(), False),
         StructField("last_name", StringType(), False),
-        StructField("gender", StringType(), False),
-        StructField("address", StringType(), False),
-        StructField("post_code", StringType(), False),
-        StructField("email", StringType(), False),
-        StructField("dob", StringType(), False),
-        StructField("username", StringType(), False),
-        StructField("registered_date", StringType(), False),
-        StructField("phone", StringType(), False),
-        StructField("picture", StringType(), False)
+        StructField("gender", StringType(), True),
+        StructField("address", StringType(), True),
+        StructField("post_code", StringType(), True),
+        StructField("dob", StringType(), True),
+        StructField("email", StringType(), True),
+        StructField("username", StringType(), True),
+        StructField("registered_date", StringType(), True),
+        StructField("phone", StringType(), True),
+        StructField("picture", StringType(), True)
     ])
 
-    # Đọc dữ liệu từ Kafka
     sel = spark_df.selectExpr("CAST(value AS STRING)") \
-        .select(from_json(col('value'), schema).alias('data')) \
-        .select("data.*")
+        .select(from_json(col('value'), schema).alias('data')).select("data.*")
+    print(sel)
 
-    # Ép kiểu dữ liệu trước khi ghi vào Cassandra
-    sel_casted = sel.withColumn("id", col("id").cast("string")) \
-        .withColumn("first_name", col("first_name").cast("string")) \
-        .withColumn("last_name", col("last_name").cast("string")) \
-        .withColumn("gender", col("gender").cast("string")) \
-        .withColumn("address", col("address").cast("string")) \
-        .withColumn("post_code", col("post_code").cast("string")) \
-        .withColumn("email", col("email").cast("string")) \
-        .withColumn("dob", col("dob").cast("string")) \
-        .withColumn("username", col("username").cast("string")) \
-        .withColumn("registered_date", col("registered_date").cast("string")) \
-        .withColumn("phone", col("phone").cast("string")) \
-        .withColumn("picture", col("picture").cast("string"))
-
-    print("DataFrame after casting:")
-    sel_casted.printSchema()
-    #     # print(sel_casted)
-    if sel_casted.isStreaming:
-        print("Spark DataFrame is streaming.")
-    else:
-        print("Spark DataFrame is not streaming or is empty.")
-
-    return sel_casted
+    return sel
 
 
 if __name__ == "__main__":
@@ -176,28 +152,43 @@ if __name__ == "__main__":
         selection_df = create_selection_df_from_kafka(spark_df)
         session = create_cassandra_connection()
 
+        if spark_df is not None:
+            # Xác minh Kafka DataFrame
+            print("Validating Kafka DataFrame...")
+            print(f"Is streaming: {spark_df.isStreaming}")  # Kết quả phải là True
+            spark_df.printSchema()
+
         if session is not None:
             create_keyspace(session)
             create_table(session)
-            # insert_data(session)
 
             print("Streaming is being started...")
+            try:
+                streaming_query = (selection_df.writeStream.format("org.apache.spark.sql.cassandra")
+                                   .option('checkpointLocation', 'C:/tmp/checkpoint')
+                                   .option('keyspace', 'spark_streams')
+                                   .option('table', 'created_users')
+                                   .start())
+                print(f"Streaming is being started 2...")
 
-            streaming_query = selection_df.writeStream \
-                            .format("org.apache.spark.sql.cassandra") \
-                            .option("checkpointLocation", "/tmp/checkpoint") \
-                            .option("keyspace", "spark_streams") \
-                            .option("table", "created_users") \
-                            .start()
+                # Kiểm tra trạng thái của query
+                print(streaming_query.status)
+                print(f"Query Status: {streaming_query.status}")
+                print(f"Recent Progress: {streaming_query.recentProgress}")
 
-            print(f"Streaming is being started 2...")
+                # Kiểm tra xem query có đang chạy không
+                if streaming_query.isActive:
+                    print("Query is running")
 
-            streaming_query.awaitTermination()
-            print("Streaming is being started 3...")
+                streaming_query.awaitTermination()
+                print("Streaming is being started 3...")
+            except Exception as e:
+                print(f"Streaming query failed due to: {e}")
 
 # docker-compose up -d
 # $env:PYSPARK_PYTHON="D:/data_engineering/venv/Scripts/python.exe"
-# spark-submit --master spark://localhost:7077 --packages com.datastax.spark:spark-cassandra-connector_2.12:3.5.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,org.apache.kafka:kafka-clients:3.5.1 spark_stream.py
+# spark-submit --master spark://localhost:7077 --packages com.datastax.spark:spark-cassandra-connector_2.12:3.5.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,org.apache.kafka:kafka-clients:3.5.1 spark_stream.py
 
 # docker exec -it cassandra cqlsh -u cassandra -p cassandra localhost 9042
 #SELECT * from spark_streams.created_users;
+# spark-submit --master spark://localhost:7077 spark_stream.py
